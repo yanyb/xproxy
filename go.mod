fd47ed9a93991bf10d9781834793a87b0aabd89c
yanyb <simpleyanb@gmail.com>
2026-05-21 20:08:42 +0800

    init

--- go.mod
+++ go.mod
@@ -13,7 +13,11 @@ require (
 require (
 	github.com/google/uuid v1.6.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/mobile v0.0.0-20260520154334-0e4426e1883d // indirect
+	golang.org/x/mod v0.36.0 // indirect
 	golang.org/x/net v0.54.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
+	golang.org/x/tools v0.45.0 // indirect
 )
 
 replace github.com/things-go/go-socks5 => ./third_party/go-socks5
--- mobile/android/MainActivity.kt
+++ mobile/android/MainActivity.kt
@@ -1,28 +1,54 @@
 package com.example.xproxy
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import xproxy.mobile.ClientConfig
 import xproxy.mobile.Mobile
 
 class MainActivity : ComponentActivity() {
     private var thread: Thread? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(TAG, "MainActivity.onCreate")
+
         val cfg = ClientConfig().apply {
             deviceID = "phone-1"
             serverAddr = "nddtech.cn:443"
-            heartbeatIntervalNs = 10_000_000_000
-            reconnectMinNs = 1_000_000_000
-            reconnectMaxNs = 60_000_000_000
-            proxyIdleTimeout = 30_000_000_000
-            maxConcurrent = 128
-            dnsCacheTTL = 30_000_000_000
+            heartbeatIntervalNs = 10_000_000_000L
+            reconnectMinNs = 1_000_000_000L
+            reconnectMaxNs = 60_000_000_000L
+            proxyIdleTimeout = 30_000_000_000L
+            maxConcurrent = 1000
+            dnsCacheTTL = 30_000_000_000L
         }
-        thread = Thread { Mobile.run(cfg, AndroidDns()) }.also { it.start() }
+
+        thread = Thread {
+            Log.i(TAG, "calling Mobile.run")
+            try {
+                Mobile.run(cfg, AndroidDns())
+                Log.i(TAG, "Mobile.run returned normally")
+            } catch (t: Throwable) {
+                Log.e(TAG, "Mobile.run threw", t)
+            }
+        }.apply {
+            name = "xproxy-run"
+            // Critical: catch errors here instead of letting the default
+            // handler kill the whole process.
+            setUncaughtExceptionHandler { th, ex ->
+                Log.e(TAG, "uncaught in ${th.name}", ex)
+            }
+            start()
+        }
     }
+
     override fun onDestroy() {
-        Mobile.stop()
+        Log.i(TAG, "MainActivity.onDestroy")
+        try { Mobile.stop() } catch (t: Throwable) { Log.e(TAG, "stop", t) }
+        thread?.interrupt()
         super.onDestroy()
     }
+
+    companion object { private const val TAG = "xproxy" }
 }
